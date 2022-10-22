0df987e3a7c89a8cef17b0ebd4e7072c9ee363a6
Paulo <pgouveia@yepkit.com>
2022-10-22 18:13:35 +0100

    formating

--- server/server.h
+++ server/server.h
@@ -14,14 +14,13 @@ namespace tcp_server {
             Server();
             Server(const unsigned short port, const int backlog_size);
             ~Server();
-			
-			/*! Start the server.
-			 *
-			 * The routing map with the mapping of the routes with the handler functions 
-			 */
+
+            /*! Start the server.
+             *
+             * The routing map with the mapping of the routes with the handler functions 
+             */
             void start(Router& router);
 
-        private:
     };
 
 }
