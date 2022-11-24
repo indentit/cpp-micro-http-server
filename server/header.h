98e3ebefe6d0625d2c3ae7786aed7b1735b35b41
indentit <blogindentit@gmail.com>
2022-11-24 11:24:41 +0000

    organize

--- server/header.h
+++ server/header.h
@@ -3,12 +3,14 @@
 #include <string>
             
 struct Header {
+	
+	static const std::map<unsigned int, std::string> http_status_table;
+	
 	std::string request_method;
 	std::string path;
 	std::string http_version;
 	unsigned int content_lenght = 0;
 	std::string cookie;
-	static const std::map<unsigned int, std::string> http_status_table;
 	unsigned int status_code = 200;
 	std::string content_type = "text/plain";
 };
